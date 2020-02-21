2e29e98fa52c1110b225a70d22511764aef2c88e
sunlee <sunlee128@gmail.com>
2020-02-21 16:49:49 +1100

    state test added

---
+++ README.md
@@ -0,0 +1,8 @@
+## Workflow
+- Set up a (very) simple React App with Jest and Enzyme
+- Used Enzyme's `shallow()` function to render a component
+- Tested that required DOM elements were rendered using `find()`
+- Tested state using Enzyme's `setState()`and `state()`
+- used `simulate` to interact with rendered elements(clicked button)
+- Tested component for updated after interaction
+- Created re-usable `setup()` and `findByTestAttr()` functions
--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-  render() {
-    return (
-      <div data-test="component-app">
-        <h1>App</h1>
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
 
+  render () {
+    return (
+      <div data-test='component-app'>
+        <h1 data-test='counter-display'>the counter is currently {this.state.counter}</h1>
+        <button
+          data-test='increment-button'
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >Increment counter
+        </button>
       </div>
-    )
+    );
   }
 }
 
